e7d6877515a407307cc466858a5ad5c6f53239d0
dylanmilesmsu <89435676+dylanmilesmsu@users.noreply.github.com>
2023-06-13 10:52:57 -0500

    Create Person.java

---
+++ Assignments/A05/java/graphvizGen/Person.java
@@ -0,0 +1,19 @@
+package vizgraph;
+
+public class Person {
+	String pid;
+	String name;
+	String gender;
+	String generation;
+	String byear;
+	String dyear;
+	String dage;
+	String myear;
+	String mage;
+	String ptype;
+	String clan;
+	String spouceid;
+	String parentid1;
+	String parentid2;
+	String parentnodeid;
+}
