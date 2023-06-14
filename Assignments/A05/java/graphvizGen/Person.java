1085d786f9c50b803624f1e4a6f22a3f96e5af61
bfce <bfce@pop-os.localdomain>
2023-06-13 19:24:26 -0500

    rename, upload jar

--- Assignments/A05/java/graphvizGen/Person.java
+++ Assignments/A05/java/graphvizGen/Person.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package graphvizGen;
 
 import java.util.List;
@@ -28,24 +27,3 @@ public class Person {
         return null;
     }
 }
-=======
-package vizgraph;
-
-public class Person {
-	String pid;
-	String name;
-	String gender;
-	String generation;
-	String byear;
-	String dyear;
-	String dage;
-	String myear;
-	String mage;
-	String ptype;
-	String clan;
-	String spouceid;
-	String parentid1;
-	String parentid2;
-	String parentnodeid;
-}
->>>>>>> e8178c2e10153f1620fffca073167d35bb0c3792
